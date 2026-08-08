a3bb8d0759d5821bce277597b1839ca0d62586f8
Alpha123456678 <ernadini266@gmail.com>
2026-08-08 05:18:04 -0700

    5th commit

--- lib/repository/widgets/uihelper.dart
+++ lib/repository/widgets/uihelper.dart
@@ -1,70 +1,99 @@
 import 'package:flutter/material.dart';
 
 class UiHelper {
- static Image CustomImage({
-  required String img,
-  double? height,
-  double? width,
-  BoxFit fit = BoxFit.contain,
-}) {
-  return Image.asset(
-    "assets/images/$img",
-    height: height,
-    width: width,
-    fit: fit,
-  );
-}
+  static Image CustomImage({
+    required String img,
+    double? height,
+    double? width,
+    BoxFit fit = BoxFit.contain,
+  }) {
+    return Image.asset(
+      "assets/images/$img",
+      height: height,
+      width: width,
+      fit: fit,
+    );
+  }
 
-  static Text CustomText(
-      {required String text,
-      required Color color,
-      required FontWeight fontweight,
-      String? fontfamily,
-      required double fontsize}) {
+  static Text CustomText({
+    required String text,
+    required Color color,
+    required FontWeight fontweight,
+    String? fontfamily,
+    required double fontsize,
+  }) {
     return Text(
       text,
       style: TextStyle(
-          fontSize: fontsize,
-          fontFamily: fontfamily ?? "regular",
-          fontWeight: fontweight,
-          color: color),
+        fontSize: fontsize,
+        fontFamily: fontfamily ?? "regular",
+        fontWeight: fontweight,
+        color: color,
+      ),
     );
   }
-  static Container CustomTextField({required TextEditingController controller}){
+
+  static Widget CustomTextField({
+    required TextEditingController controller,
+  }) {
     return Container(
-      height: 40,
+      height: 45,
       width: 360,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
         color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
         border: Border.all(
-          color: Color(0XFFC5C5C5)
-        )
+          color: const Color(0XFFC5C5C5),
+        ),
       ),
       child: TextField(
         controller: controller,
         decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 10),
           hintText: "Search 'ice-cream'",
-          prefixIcon: Image.asset("assets/images/search.png"),
-          suffixIcon: Image.asset("assets/images/mic 1.png"),
-          border: InputBorder.none
+          prefixIcon: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Image.asset(
+              "assets/images/search.png",
+            ),
+          ),
+          suffixIcon: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Image.asset(
+              "assets/images/mic 1.png",
+            ),
+          ),
+          border: InputBorder.none,
         ),
       ),
     );
   }
 
-  static Container CustomButton(VoidCallback callback){
-    return Container(
-      height: 18,
-      width: 30,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(
-          color: Color(0XFF27AF34)
+  static Widget CustomButton(VoidCallback callback) {
+    return InkWell(
+      onTap: callback,
+      borderRadius: BorderRadius.circular(4),
+      child: Container(
+        height: 24,
+        width: 45,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+            color: const Color(0XFF27AF34),
+          ),
+          borderRadius: BorderRadius.circular(4),
         ),
-        borderRadius: BorderRadius.circular(4)
+        child: const Center(
+          child: Text(
+            "ADD",
+            style: TextStyle(
+              fontSize: 10,
+              fontWeight: FontWeight.bold,
+              color: Color(0XFF27AF34),
+            ),
+          ),
+        ),
       ),
-      child: Center(child: Text("Add",style: TextStyle(fontSize: 8,color: Color(0XFF27AF34)),),),
     );
   }
 }
